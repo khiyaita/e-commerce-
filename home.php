e358409f737732fd0f2f6a4ed250957252629c47
khiyaita <135702055+khiyaita@users.noreply.github.com>
2024-03-30 13:04:56 +0000

    Update home.php

--- home.php
+++ home.php
@@ -1,18 +1,4 @@
-<?php
 
-include 'components/connect.php';
-
-session_start();
-
-if(isset($_SESSION['user_id'])){
-   $user_id = $_SESSION['user_id'];
-}else{
-   $user_id = '';
-};
-
-include 'components/wishlist_cart.php';
-
-?>
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -27,13 +13,10 @@ include 'components/wishlist_cart.php';
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
-   <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css">
-
 </head>
 <body>
    
-<?php include 'components/user_header.php'; ?>
+
 
 <div class="home-bg">
 
@@ -144,59 +127,6 @@ include 'components/wishlist_cart.php';
 
 </section>
 
-<section class="home-products">
-
-   <h1 class="heading">derniers produits</h1>
-
-   <div class="swiper products-slider">
-
-   <div class="swiper-wrapper">
-
-   <?php
-     $select_products = $conn->prepare("SELECT * FROM `produits` LIMIT 6"); 
-     $select_products->execute();
-     if($select_products->rowCount() > 0){
-      while($fetch_product = $select_products->fetch(PDO::FETCH_ASSOC)){
-   ?>
-   <form action="" method="post" class="swiper-slide slide">
-      <input type="hidden" name="pid" value="<?= $fetch_product['id']; ?>">
-      <input type="hidden" name="name" value="<?= $fetch_product['nom']; ?>">
-      <input type="hidden" name="price" value="<?= $fetch_product['prix']; ?>">
-      <input type="hidden" name="image" value="<?= $fetch_product['image_01']; ?>">
-      <button class="fas fa-heart" type="submit" name="add_to_wishlist"></button>
-      <a href="quick_view.php?pid=<?= $fetch_product['id']; ?>" class="fas fa-eye"></a>
-      <img src="uploaded_img/<?= $fetch_product['image_01']; ?>" alt="">
-      <div class="name"><?= $fetch_product['nom']; ?></div>
-      <div class="flex">
-         <div class="price"><span>$</span><?= $fetch_product['prix']; ?><span>/-</span></div>
-         <input type="number" name="qty" class="qty" min="1" max="99" onkeypress="if(this.value.length == 2) return false;" value="1">
-      </div>
-      <input type="submit" value="ajouter au panier" class="btn" name="add_to_cart">
-   </form>
-   <?php
-      }
-   }else{
-      echo '<p class="empty">aucun produit ajouté pour le moment !</p>';
-   }
-   ?>
-
-   </div>
-
-   <div class="swiper-pagination"></div>
-
-   </div>
-
-</section>
-
-
-
-
-
-
-
-
-
-<?php include 'components/footer.php'; ?>
 
 <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
 
